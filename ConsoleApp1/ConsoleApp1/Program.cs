a9027e87f9877ecf6384dea4f2491e0a254a2f5e
ASUS <ASUS@DESKTOP-Q4LS08M>
2019-05-30 20:21:01 +0400

    ждал оливье.

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime нг = new DateTime(2020, 1, 1);
+            DateTime сейчас = DateTime.Now;
+
+            TimeSpan tsp = нг - сейчас;
+            int days = (int)tsp.TotalDays;
+
+            Console.WriteLine(" Сколько ещё ждать оливье??? - " + days);
+            Console.ReadKey();
+        }
+    }
+}
